7a81c1c8f4b8d21b07c7d9471bad06c62617a79d
WasfaParwez <wasfa123@gmail.com>
2023-04-21 02:32:41 +0530

    get API assignment 2

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,52 +1,60 @@
 const express = require('express');
 const { route } = require('express/lib/application');
 const router = express.Router();
-const commonFile = require('./common')
-const myUnderscore = require('underscore')
 
-router.get('/test-me', function (req, res) {
-    res.send('This should be working!')
-});
 
-router.get('/test-you', function (req, res) {
-    console.log('The exported module is: ',commonFile)
-    commonFile.doSomething()
-    console.log('This is the constant I created', commonFile.name)
-    res.send('Hello there, welcome to this application!')
-});
-
-router.get('/test-underscore', function(req, res){
-    let result = myUnderscore.first([11,12,23,44,15], 4)
-    console.log('the result is',result)
-    res.send('done')
+router.get('/movies', function(req, res){
+  res.send(['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    );
 })
 
-router.get('/cohorts', function (request, response){
-    // logic to get the cohorts from database
-    // logic tp get only the active cohorts
-    // logic to get only the cohort with a size than 50
-    // logic to get only the backend cohorts
-    response.send(['technetium','nobelium'])
+
+const movies=['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+
+
+router.get('/movies/:indexN',function(req, res){
+  const indexN=req.params.indexN;c
+  const movie=movies[indexN];
+  if (movies.length -1 < indexN || indexN < 0) {
+    res.send("404 use a valid index number")}
+  else {
+    res.send(movie)
+  }
+
 })
 
-router.get('/students', function(req, res){
-    // receive or access the query params in the code
-    // write a logic on these query params
-    // city, score
-    console.log(req.query)
-    let requestedCity = req.query.city
-    let sortField = req.query.sort
-    // logic to get students
-    res.send(["Sabiha","Neha","Akash","Sonali"])
+
+
+const films = [
+  {
+    id: 1,
+    name: 'The Shining'
+  },
+  {
+    id: 2,
+    name: 'Incendies'
+  },
+  {
+    id: 3,
+    name: 'Rang de Basanti'
+  },
+  {
+    id: 4,
+    name: 'Finding Nemo'
+  }
+];
+
+router.get('/films', function(req, res){
+  res.send(films);
 })
 
-router.get('/students/:studentName', function(req, res) {
-    console.log(req.params.studentName)
-    /// go to database and search for studentName student
-    // store the data found in this variable - studentDetails
-    //res.send({data: studentDetails})
-    res.send('student data')
-})
+router.get('/films/:id', function(req, res){
+  const idx = req.params.id;
+  film= films.find(element=>element.id==idx);
+  res.send(film);})
+
+  
+
 
 
 module.exports = router;
