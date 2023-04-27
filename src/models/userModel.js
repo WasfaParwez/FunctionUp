bd6b545144e5efc39d5e8d3a87eb5c02713899c9
WasfaParwez <wasfa123@gmail.com>
2023-04-27 12:10:39 +0530

    Mongo-DB

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,29 +1,19 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
+const bookSchema = new mongoose.Schema( {
+    bookName: {type : 'string',
+                required: true},
+    authorName: {type : 'string',
+    required: true},
+    category: {
         type: String,
-        unique: true,
-        required: true
+        enum: ["adventure","romance","fanatasy","self-help","autoBiography"] 
     },
-    emailId: String,
-    gender: {
-        type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
-    },
-    age: Number,
-    // isIndian: Boolean,
-    // parentsInfo: {
-    //     motherName: String,
-    //     fatherName: String,
-    //     siblingName: String
-    // },
-    // cars: [ String  ]
+    year: Number,
+
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema) //users
+module.exports = mongoose.model('book', bookSchema) //users
 
 
 
